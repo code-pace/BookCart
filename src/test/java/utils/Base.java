c88c8870fe44ee9253ee1c1c3221aee3a3a626d6
code-pace <harrisonanyika@gmail.com>
2023-10-18 00:18:39 +0100

    cart page test update

--- src/test/java/utils/Base.java
+++ src/test/java/utils/Base.java
@@ -8,17 +8,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Marker;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
-
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.time.Duration;
@@ -51,27 +48,44 @@ public class Base {
     }
 
     @BeforeClass
-    @Parameters("browser")
-    public void setUp(String browser) throws Exception {
+    @Parameters({"browser","executeMode"})
+    public void setUp(String browser, String executeMode) throws Exception {
         createTest();
-        if(browser.equalsIgnoreCase("chrome")) {
-//            WebDriverManager.chromedriver().clearDriverCache().setup();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--disable-dev-shm-usage");
-            webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-//            webDriver = new ChromeDriver(options);
-        }
-        else if(browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().clearDriverCache().setup();
-            DesiredCapabilities dc = new DesiredCapabilities();
-            dc.setBrowserName(browser);
-            webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
-//            webDriver = new FirefoxDriver();
+        if(executeMode.equalsIgnoreCase("remote")) {
+            if(browser.equalsIgnoreCase("chrome")) {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-dev-shm-usage");
+                webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+            }
+            else if(browser.equalsIgnoreCase("firefox")) {
+                DesiredCapabilities dc = new DesiredCapabilities();
+                dc.setBrowserName(browser);
+                webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
+            }
+            else {
+                throw new Exception("Browser not recognized");
+            }
         }
         else {
-            throw new Exception("Browser not recognized");
+            if(browser.equalsIgnoreCase("chrome")) {
+                WebDriverManager.chromedriver().clearDriverCache().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-dev-shm-usage");
+                webDriver = new ChromeDriver(options);
+            }
+            else if(browser.equalsIgnoreCase("firefox")) {
+                WebDriverManager.firefoxdriver().clearDriverCache().setup();
+                DesiredCapabilities dc = new DesiredCapabilities();
+                dc.setBrowserName(browser);
+                webDriver = new FirefoxDriver();
+            }
+            else {
+                throw new Exception("Browser not recognized");
+            }
         }
+
         driver.set(webDriver);
         Duration time = Duration.ofMillis(3000);
         wait = new WebDriverWait(webDriver, time);
